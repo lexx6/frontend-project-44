b174ba50e231dd18a1a87f7a97513fd995da5d40
lexx696 <lexx696@yandex.ru>
2024-03-04 12:25:35 +0300

    refactoring

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,15 +3,15 @@ import start from '../engine.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const gcd = (number1, number2) => {
+const getGcd = (number1, number2) => {
   if (!number2) return number1;
-  return gcd(number2, number1 % number2);
+  return getGcd(number2, number1 % number2);
 };
 
 const getQuestionAnswer = () => {
   const number1 = getRandomArbitrary(1, 20);
   const number2 = getRandomArbitrary(1, 20);
-  const answer = gcd(number1, number2);
+  const answer = getGcd(number1, number2);
   return [`${number1} ${number2}`, answer];
 };
 
