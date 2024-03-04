702b895494c41d29984114fd206c4820c6118a50
lexx696 <lexx696@yandex.ru>
2024-03-04 11:54:53 +0300

    refactoring

--- src/engine.js
+++ src/engine.js
@@ -2,14 +2,14 @@ import readlineSync from 'readline-sync';
 
 const triesCount = 3;
 
-export default (description, questionAnswer) => {
+export default (description, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(description);
 
   for (let i = 0; i < triesCount; i += 1) {
-    const [question, answer] = questionAnswer();
+    const [question, answer] = generateRound();
     console.log(`Question: ${question}`);
     const input = readlineSync.question('Your answer: ');
     if (answer.toString() !== input) {
